301328495ef9d97a46c5a81debe07cc19b0f3c62
HexTecGames <Patrick.Hellwig93@gmail.com>
2024-07-24 19:30:50 +0200

    moved files to different package

--- Runtime/Enums.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace HexTecGames
-{
-    public enum UpdateType { None, Minor, Medium, Major }
-    public enum VersionType { Full, Demo }
-}
