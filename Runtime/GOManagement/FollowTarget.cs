14c7e96b5983c27a088a97065a975fe660825791
HexTecGames <Patrick.Hellwig93@gmail.com>
2025-04-03 01:46:19 +0200

    FollowTarget improvements

--- Runtime/GOManagement/FollowTarget.cs
+++ Runtime/GOManagement/FollowTarget.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using HexTecGames.EaseFunctions;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace HexTecGames.Basics
 {
-	/// <summary>
+    /// <summary>
     /// Sets this objects position equal to the target's position + offset.
     /// </summary>
     public class FollowTarget : MonoBehaviour
-	{
+    {
         public Transform Target
         {
             get
@@ -21,21 +22,64 @@ namespace HexTecGames.Basics
                 target = value;
             }
         }
+
+        public Vector3 Offset
+        {
+            get
+            {
+                return this.offset;
+            }
+            private set
+            {
+                this.offset = value;
+            }
+        }
+
         [SerializeField, FormerlySerializedAs("Target")] private Transform target = default;
         [SerializeField, FormerlySerializedAs("Offset")] private Vector3 offset = default;
 
 
         private void Reset()
         {
-            offset = transform.position;
+            Offset = transform.position;
         }
 
         private void Update()
         {
             if (target != null)
             {
-                transform.position = target.transform.position + offset;
-            }           
+                transform.position = target.transform.position + Offset;
+            }
+        }
+
+        public void SetOffset(Vector3 offset)
+        {
+            this.Offset = offset;
+        }
+        public void SetOffset(Vector3 offset, float duration, EaseFunction easeFunction)
+        {
+            StartCoroutine(AnimateOffsetChange(this.Offset, offset, duration, easeFunction));
+        }
+
+        public void AddOffset(Vector3 offsetToAdd)
+        {
+            this.Offset += offsetToAdd;
+        }
+        public void AddOffset(Vector3 offsetToAdd, float duration, EaseFunction easeFunction)
+        {
+            StartCoroutine(AnimateOffsetChange(this.Offset, this.Offset + offsetToAdd, duration, easeFunction));
+        }
+        private IEnumerator AnimateOffsetChange(Vector3 startOffset, Vector3 endOffset, float duration, EaseFunction easeFunction)
+        {
+            float timer = 0;
+            while (timer < duration)
+            {
+                float progress = easeFunction.GetValue(timer / duration);
+                this.Offset = Vector3.Lerp(startOffset, endOffset, progress);
+                yield return null;
+                timer += Time.deltaTime;
+
+            }
         }
     }
 }
