c7c5ea43e0f2af08f9a443d4069bfc157e8ea3fe
HexTecGames <Patrick.Hellwig93@gmail.com>
2025-10-23 15:18:57 +0200

    added SelectableAnimator

---
+++ Runtime/Effects/SelectableAnimator.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using HexTecGames.EaseFunctions;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace HexTecGames.Basics
+{
+    public class SelectableAnimator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private EaseFunction easeFunction = new EaseFunction(EasingType.EaseInOut, FunctionType.Quad);
+        [SerializeField] private float speed = 10f;
+
+        [SerializeField] private Vector3 startSize = Vector3.one;
+        [SerializeField] private Vector3 endSize = new Vector3(1.05f, 1.05f, 1.05f);
+
+        private float progress;
+        private Coroutine scaleRoutine;
+
+        private void OnDisable()
+        {
+            progress = 0;
+        }
+
+        private IEnumerator AnimateScale(bool hover)
+        {
+            float t = progress;
+            float direction = hover ? 1f : -1f;
+
+            while ((hover && t < 1f) || (!hover && t > 0f))
+            {
+                t += Time.deltaTime * speed * direction;
+                t = Mathf.Clamp01(t);
+                transform.localScale = Vector3.Lerp(startSize, endSize, easeFunction.GetValue(t));
+                progress = t;
+                yield return null;
+            }
+
+            scaleRoutine = null;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (scaleRoutine != null) StopCoroutine(scaleRoutine);
+            scaleRoutine = StartCoroutine(AnimateScale(true));
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (scaleRoutine != null) StopCoroutine(scaleRoutine);
+            scaleRoutine = StartCoroutine(AnimateScale(false));
+        }
+    }
+}
