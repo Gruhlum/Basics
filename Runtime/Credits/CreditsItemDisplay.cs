2f68ca89602597b10bc5ac27f4163a875fc9b5b1
HexTecGames <Patrick.Hellwig93@gmail.com>
2025-12-22 05:29:37 +0100

    fixed CreditItemDisplay

--- Runtime/Credits/CreditsItemDisplay.cs
+++ Runtime/Credits/CreditsItemDisplay.cs
@@ -7,21 +7,19 @@ namespace HexTecGames.Basics.Credits
 {
     public class CreditsItemDisplay : Display<CreditsItemDisplay, CreditsItem>
     {
-        [SerializeField] private TMP_Text nameGUI = default;
         [SerializeField] private TMP_Text roleGUI = default;
+        [SerializeField] private TMP_Text nameGUI = default;
         [SerializeField] private LinkButton linkButton = default;
 
-
-        protected void Reset()
-        {
-            linkButton = GetComponent<LinkButton>();
-        }
 
         protected override void DrawItem(CreditsItem item)
         {
             nameGUI.text = item.name;
             roleGUI.text = item.role;
-            linkButton.URL = item.itchURL;
+            if (linkButton != null)
+            {
+                linkButton.URL = item.itchURL;
+            }
             name = $"CreditsDisplay ({item.name})";
         }
     }
